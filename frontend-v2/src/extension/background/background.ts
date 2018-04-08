c8ecd1d6bdf0610b97ba7f2c6db1b56b72dbc7f3
Grievouz <ronke.tammo@gmail.com>
2018-04-08 14:23:46 +0200

    Change sortyBy component

--- frontend-v2/src/extension/background/background.ts
+++ frontend-v2/src/extension/background/background.ts
@@ -2,39 +2,27 @@ import * as Browserium from "../../utils/Browserium";
 import * as Dummy from "./dummy";
 
 
-
 export enum Msg{
-    recentSuggestions = "recentSuggestions"
+    test = "test"
 }
 
-export 
+
 
 // VARIABLES
 const backceiver: Browserium.Backceiver = new Browserium.Backceiver("background");
-let suggestions: { [identifier: string]: any } = {} // any is CardData
-let recentSuggestions: string[] = [];
 
 
 
 // QUEUE
-let queue: { [index: string]: string[] } = {};
-let addRequestToQueue = (request: string, sender: string) => {
-    if(!queue[request])
-            queue[request] = [];
-
-    queue[request].push(sender)
-}
 
 
 
 // LISTENERS
+backceiver.addListener(Msg.test, () => console.log("test"));
 
 
 
 // REQUESTS
-let requestRecentSuggestions = () => {
-    
-}
 
 
 
@@ -43,9 +31,6 @@ let requestRecentSuggestions = () => {
 
 
 // UPDATE
-let updateRecentSuggestions = () => {
-    //sends the new recentSugessstinos array to the backend
-}
 
 
 
--- frontend-v2/src/sidebar/app/app.tsx
+++ frontend-v2/src/sidebar/app/app.tsx
@@ -132,6 +132,32 @@ class App extends React.Component {
 			"id": "5ac7dad753c12b234dc700d37"
 		}
 	]
+	private bookedCards: CardData[] = [
+		{
+			"provider": "Medium",
+			"url": "https://medium.com/@saginadir/why-i-love-golang-90085898b4f7",
+			"kind": "article",
+			"title": "Why I Love Golang",
+			"category": "go",
+			"tags": [
+				"programming"
+			],
+			"content": "I love the Go programming language, or as some refer to it, Golang. It’s simple and it’s great.\r\n\r\nI write this on a tangent. Didn’t expect Golang to be so good.\r\n\r\nI first picked up go around January 2016, it had a relative small but enthusiastic community here in Israel.\r\n\r\nI didn’t think much of it at the time, I was honing my programming skills and Golang was just a tool I’ve used to accomplish a task.\r\n\r\nEven one year ago, using go was brilliant. The process was straightforward once I’ve got the general hang of the language.\r\n\r\nI wrote a crucial piece of code for Visualead, the company I work for, and it didn’t let us down, still running in production a year later with zero maintenance since then.\r\n\r\nRecently I found myself again using Golang again, and I felt compelled to write about the reasons I fell in love with Golang.\r\n\r\nThe GOPATH environment\r\nThis is one of the first things you’ll have to handle once you begin writing in Go.\r\n\r\nSetup your GOPATH directory anywhere on your computer, complete with bin, src, and pkg directories and you are ready to begin writing.\r\n\r\n",
+			"id": "5ac7dad753c12b0fdc700d35"
+		},
+		{
+			"provider": "Medium",
+			"url": "https://medium.com/@saginadir/why-i-love-golang-90085898b4f7",
+			"kind": "article",
+			"title": "Why I Love Golang 3",
+			"category": "test",
+			"tags": [
+				"programming"
+			],
+			"content": "I love the Go programming language, or as some refer to it, Golang. It’s simple and it’s great.\r\n\r\nI write this on a tangent. Didn’t expect Golang to be so good.\r\n\r\nI first picked up go around January 2016, it had a relative small but enthusiastic community here in Israel.\r\n\r\nI didn’t think much of it at the time, I was honing my programming skills and Golang was just a tool I’ve used to accomplish a task.\r\n\r\nEven one year ago, using go was brilliant. The process was straightforward once I’ve got the general hang of the language.\r\n\r\nI wrote a crucial piece of code for Visualead, the company I work for, and it didn’t let us down, still running in production a year later with zero maintenance since then.\r\n\r\nRecently I found myself again using Golang again, and I felt compelled to write about the reasons I fell in love with Golang.\r\n\r\nThe GOPATH environment\r\nThis is one of the first things you’ll have to handle once you begin writing in Go.\r\n\r\nSetup your GOPATH directory anywhere on your computer, complete with bin, src, and pkg directories and you are ready to begin writing.\r\n\r\n",
+			"id": "5ac7dad753c12b234dc700d37"
+		}
+	]
 
 	componentDidMount(){
 		
@@ -144,7 +170,7 @@ class App extends React.Component {
 					identifier={value.id}
 					category={value.category}
 					tags={value.tags}
-					bookmarked={true}
+					bookmarked={false}
 					provider={value.provider}
 
 					icon={<img src={"https://www.google.com/s2/favicons?domain_url=" + value.url} />}
@@ -157,7 +183,7 @@ class App extends React.Component {
 			);
 		});
 
-		this.recentCards.map((value: CardData) => {
+		this.bookedCards.map((value: CardData) => {
 			PagesStore.addBookmark(
 				<Card.default
 					key={Math.random()}
@@ -166,7 +192,6 @@ class App extends React.Component {
 					tags={value.tags}
 					bookmarked={true}
 					provider={value.provider}
-
 					icon={<img src={"https://www.google.com/s2/favicons?domain_url=" + value.url} />}
 					body={{
 						headline: value.title,
